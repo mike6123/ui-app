1309e63bafce527cce158971845e36ed2de8a776
xiaomou <2161068705@qq.com>
2025-12-07 11:30:11 +0800

    Delete 新建文件夹 directory

--- 新建文件夹/my-express-project/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-
-// 搜索文章（无需认证）
-router.get('/article', searchController.searchArticle);
-
-module.exports = router;
-
